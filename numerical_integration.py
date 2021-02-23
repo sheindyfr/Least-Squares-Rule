848b5b979faed3d89f4c7a93308ff0a882870034
sheindyfr <sheindy33@gmail.com>
2021-02-23 19:55:22 +0200

    add numerical integration methods

---
+++ numerical_integration.py
@@ -0,0 +1,46 @@
+import math
+
+# ------------------------------------------------------------
+# Calculate numerical integration for f(x) using trapezoids
+# @params:  f =>    the function to be integrated
+#           n =>    number of trapezoids   
+#           a, b => interval bounds
+# ------------------------------------------------------------
+def trapezoid_rule(f, n, a, b):
+    h = (b - a) / float(n)
+    integral = 0.5 * h * (f(a) + f(b))
+
+    for i in range(1, int(n)):
+        integral = integral + h * f(a + i * h)
+
+    return integral
+
+# ------------------------------------------------------------
+# Calculate numerical integration for f(x) using Simpson
+# @params:  f =>    the function to be integrated
+#           n =>    number of subintervals (must be even)   
+#           a, b => interval bounds
+# ------------------------------------------------------------
+def simpson_rule(f, n, a, b):
+    if n % 2:
+        raise ValueError("n must be even (received n=%d)" % n)
+
+    h = (b - a) / n
+    s = f(a) + f(b)
+
+    for i in range(1, n, 2):
+        s += 4 * f(a + i * h)
+    for i in range(2, n - 1, 2):
+        s += 2 * f(a + i * h)
+
+    return s * h / 3
+
+
+EPSILON = 1e-6
+a = -10
+b = 10
+n = 100
+f = lambda x : math.exp(-x**2)
+
+print(trapezoid_rule(f, n, a, b))
+print(simpson_rule(f, n, a, b))
